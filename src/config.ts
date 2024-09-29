d780948727b54d6a9553d81eb5d67f6734931e4e
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-09-29 17:25:54 +0300

    updated config struct

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,35 @@
+import path from "node:path";
+
 export default {
   domain: "notpx.app",
-  origin: "https://image.notpx.app",
+  origin: "https://app.notpx.app",
   configDomain: "npx-cdn.fra1.cdn.digitaloceanspaces.com",
-  userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
-  auth: [],
   proxy: [],
+  apiId: Bun.env.API_ID,
+  apiHash: Bun.env.API_HASH,
+  maxLifetime: 1700, // seconds (real lifetime is 30 mins, but we get only ~28 mins to prevent errors)
+  sessionsFolder: path.join(__dirname, "..", "sessions"),
+  screenshotsFolder: path.join(__dirname, "..", "map"),
+  referalId: Bun.env.REFERAL_ID ?? "f587778212",
   autoUpgrade: true,
   useFastRecharge: true, // fast recharge by goods
   checkPixelInfo: true,
   setPixelsToMap: true,
-  delay: 5, // seconds
-  claimDelay: 3600, // seconds
+  screenMapDelay: 30, // seconds
+  requestsDelay: {
+    // seconds
+    min: 0.1,
+    max: 0.5,
+  },
+  runDelay: {
+    // seconds
+    onStart: 0, // Delay before run bot
+    min: 3,
+    max: 15,
+  },
+  claimDelay: {
+    // seconds
+    min: 1200,
+    max: 4800,
+  },
 };
